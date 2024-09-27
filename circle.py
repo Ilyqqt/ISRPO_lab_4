a51e170b522c68af85fff8b80d92b00edd0c83d5
Ilyqqt <zxcvbnm.iliya@gmail.com>
2024-09-28 00:01:07 +0300

    added description for functions square.py and circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,19 @@
 import math
+<<<<<<< HEAD
 def circle_area(r):
+=======
+
+
+def area(r):
+>>>>>>> 3698d64 (added description for functions square.py and circle.py)
     ''' Возвращает площадь окружности с радиусом = а '''
     return math.pi * r * r
 
 
+<<<<<<< HEAD
 def circle_perimeter(r):
+=======
+def perimeter(r):
+>>>>>>> 3698d64 (added description for functions square.py and circle.py)
     ''' Возращает длину окружности с радиусом = а '''
     return 2 * math.pi * r
-
--- square.py
+++ square.py
@@ -1,8 +1,16 @@
 
+<<<<<<< HEAD
 def square_area(a):
     ''' Возращает площадь квадрата со стороной = а '''
     return a * a
 
 def square_perimeter(a):
+=======
+def area(a):
+    ''' Возращает площадь квадрата со стороной = а '''
+    return a * a
+
+def perimeter(a):
+>>>>>>> 3698d64 (added description for functions square.py and circle.py)
     ''' Возращает периметр квадрата со стороной = а '''
     return 4 * a
